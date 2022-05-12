6e76f4da4d528f7ddbeb4411ecb6e39087043433
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:42 -0300

    chore: re index vidhdr

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."vidhdr-master".dir   = "master";
+  inputs."vidhdr-master".owner = "nim-nix-pkgs";
+  inputs."vidhdr-master".ref   = "master";
+  inputs."vidhdr-master".repo  = "vidhdr";
+  inputs."vidhdr-master".type  = "github";
+  inputs."vidhdr-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."vidhdr-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
